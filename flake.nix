0b6c8fe49724831da185bbfd6e7a7cae9a256ccf
Minerofmillions <jasoncollins818@gmail.com>
2026-09-23 18:18:49 -0400

    Start NixOS integration

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "tModLoader development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        packages = with pkgs; [
+          dotnetCorePackages.sdk_8_0
+          fna3d
+        ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = packages;
+
+          shellHook = ''
+            export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath packages}:$LD_LIBRARY_PATH"
+            echo "tModLoader development shell loaded."
+          '';
+        };
+      });
+}
