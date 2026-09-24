cd2397fdd135a223b20d44f1a93e170bc3fbe112
Minerofmillions <jasoncollins818@gmail.com>
2026-09-24 12:35:31 -0400

    Update coin drop rule.

--- JsonLoot.cs
+++ JsonLoot.cs
@@ -10,10 +10,21 @@ namespace RecipeSaver;
 
 public class JsonLoot
 {
-    [UsedImplicitly] public readonly IDictionary<int, IDictionary<string, float>> globalLoot = new SortedDictionary<int, IDictionary<string, float>>();
-    [UsedImplicitly] public readonly IDictionary<int, IDictionary<string, float>> normalLoot = new SortedDictionary<int, IDictionary<string, float>>();
-    [UsedImplicitly] public readonly IDictionary<int, IDictionary<string, float>> expertLoot = new SortedDictionary<int, IDictionary<string, float>>();
-    [UsedImplicitly] public readonly IDictionary<int, IDictionary<string, float>> masterLoot = new SortedDictionary<int, IDictionary<string, float>>();
+    [UsedImplicitly]
+    public readonly IDictionary<int, IDictionary<string, float>> globalLoot =
+        new SortedDictionary<int, IDictionary<string, float>>();
+
+    [UsedImplicitly]
+    public readonly IDictionary<int, IDictionary<string, float>> normalLoot =
+        new SortedDictionary<int, IDictionary<string, float>>();
+
+    [UsedImplicitly]
+    public readonly IDictionary<int, IDictionary<string, float>> expertLoot =
+        new SortedDictionary<int, IDictionary<string, float>>();
+
+    [UsedImplicitly]
+    public readonly IDictionary<int, IDictionary<string, float>> masterLoot =
+        new SortedDictionary<int, IDictionary<string, float>>();
 
     private bool GlobalEmpty => DictIsEmpty(globalLoot);
     private bool NormalEmpty => DictIsEmpty(normalLoot);
@@ -21,28 +32,37 @@ public class JsonLoot
     private bool MasterEmpty => DictIsEmpty(masterLoot);
     private bool IsEmpty => GlobalEmpty && NormalEmpty && ExpertEmpty && MasterEmpty;
 
-    private static void AddItemToDict(IDictionary<int, IDictionary<string, float>> dict, string condition, int itemId, int numerator, int denominator, int min, int max, float inheritedChance = 1f) =>
+    private static void AddItemToDict(IDictionary<int, IDictionary<string, float>> dict, string condition, int itemId,
+        int numerator, int denominator, int min, int max, float inheritedChance = 1f) =>
         AddItemToDict(dict, condition, itemId, inheritedChance * numerator / denominator, min, max);
-    private static void AddItemToDict(IDictionary<int, IDictionary<string, float>> dict, string condition, int itemId, float chance, int min, int max)
+
+    private static void AddItemToDict(IDictionary<int, IDictionary<string, float>> dict, string condition, int itemId,
+        float chance, int min, int max)
     {
         if (!dict.TryGetValue(itemId, out var conditionalLoot))
         {
             conditionalLoot = new SortedDictionary<string, float>();
             dict[itemId] = conditionalLoot;
         }
+
         if (!conditionalLoot.TryGetValue(condition, out var currentAverage)) currentAverage = 0f;
         conditionalLoot[condition] = currentAverage + chance * (min + max) / 2f;
     }
 
     public void AddRule(IItemDropRule rule) => AddRule(rule, globalLoot, 1f, "");
-    private void AddRule(IItemDropRule rule, IDictionary<int, IDictionary<string, float>> dict, float inheritedChance, string condition)
+
+    private void AddRule(IItemDropRule rule, IDictionary<int, IDictionary<string, float>> dict, float inheritedChance,
+        string condition)
     {
         string addedCondition;
         string newCondition;
         switch (rule)
         {
             case CommonDropWithRerolls c:
-                AddItemToDict(dict, condition, c.itemId, inheritedChance * (1 - MathF.Pow(1 - (float)c.chanceNumerator / c.chanceDenominator, c.timesToRoll)), c.amountDroppedMinimum, c.amountDroppedMaximum);
+                AddItemToDict(dict, condition, c.itemId,
+                    inheritedChance *
+                    (1 - MathF.Pow(1 - (float)c.chanceNumerator / c.chanceDenominator, c.timesToRoll)),
+                    c.amountDroppedMinimum, c.amountDroppedMaximum);
                 c.ChainedRules.ForEach(r => AddRule(r.RuleToChain, dict, inheritedChance, condition));
                 break;
             case ItemDropWithConditionRule i:
@@ -50,28 +70,35 @@ public class JsonLoot
                 if (condition == "") newCondition = addedCondition;
                 else if (addedCondition == "") newCondition = condition;
                 else newCondition = condition + "&&" + addedCondition;
-                AddItemToDict(dict, newCondition, i.itemId, i.chanceNumerator, i.chanceDenominator, i.amountDroppedMinimum, i.amountDroppedMaximum, inheritedChance);
+                AddItemToDict(dict, newCondition, i.itemId, i.chanceNumerator, i.chanceDenominator,
+                    i.amountDroppedMinimum, i.amountDroppedMaximum, inheritedChance);
                 i.ChainedRules.ForEach(r => AddRule(r.RuleToChain, dict, inheritedChance, newCondition));
                 break;
             case OneFromOptionsDropRule o:
                 foreach (var dropId in o.dropIds)
                 {
-                    AddItemToDict(dict, condition, dropId, o.chanceNumerator, o.chanceDenominator * o.dropIds.Length, 1, 1, inheritedChance);
+                    AddItemToDict(dict, condition, dropId, o.chanceNumerator, o.chanceDenominator * o.dropIds.Length, 1,
+                        1, inheritedChance);
                 }
+
                 o.ChainedRules.ForEach(r => AddRule(r.RuleToChain, dict, inheritedChance, condition));
                 break;
             case OneFromOptionsNotScaledWithLuckDropRule o:
                 foreach (var dropId in o.dropIds)
                 {
-                    AddItemToDict(dict, condition, dropId, o.chanceNumerator, o.chanceDenominator * o.dropIds.Length, 1, 1, inheritedChance);
+                    AddItemToDict(dict, condition, dropId, o.chanceNumerator, o.chanceDenominator * o.dropIds.Length, 1,
+                        1, inheritedChance);
                 }
+
                 o.ChainedRules.ForEach(r => AddRule(r.RuleToChain, dict, inheritedChance, condition));
                 break;
             case OneFromRulesRule o:
                 foreach (var r in o.options)
                 {
-                    AddRule(r, dict, inheritedChance * o.chanceNumerator / (o.chanceDenominator * o.options.Length), condition);
+                    AddRule(r, dict, inheritedChance * o.chanceNumerator / (o.chanceDenominator * o.options.Length),
+                        condition);
                 }
+
                 o.ChainedRules.ForEach(r => AddRule(r.RuleToChain, dict, inheritedChance, condition));
                 break;
             case SequentialRulesRule s:
@@ -79,6 +106,7 @@ public class JsonLoot
                 {
                     AddRule(r, dict, inheritedChance, condition);
                 }
+
                 s.ChainedRules.ForEach(r => AddRule(r.RuleToChain, dict, inheritedChance, condition));
                 break;
             case SequentialRulesNotScalingWithLuckRule s:
@@ -86,20 +114,25 @@ public class JsonLoot
                 {
                     AddRule(r, dict, inheritedChance, condition);
                 }
+
                 s.ChainedRules.ForEach(r => AddRule(r.RuleToChain, dict, inheritedChance, condition));
                 break;
             case FewFromOptionsDropRule f:
                 foreach (var dropId in f.dropIds)
                 {
-                    AddItemToDict(dict, condition, dropId, f.chanceNumerator * f.amount, f.chanceDenominator * f.dropIds.Length, 1, 1, inheritedChance);
+                    AddItemToDict(dict, condition, dropId, f.chanceNumerator * f.amount,
+                        f.chanceDenominator * f.dropIds.Length, 1, 1, inheritedChance);
                 }
+
                 f.ChainedRules.ForEach(r => AddRule(r.RuleToChain, dict, inheritedChance, condition));
                 break;
             case FewFromOptionsNotScaledWithLuckDropRule f:
                 foreach (var dropId in f.dropIds)
                 {
-                    AddItemToDict(dict, condition, dropId, f.chanceNumerator * f.amount, f.chanceDenominator * f.dropIds.Length, 1, 1, inheritedChance);
+                    AddItemToDict(dict, condition, dropId, f.chanceNumerator * f.amount,
+                        f.chanceDenominator * f.dropIds.Length, 1, 1, inheritedChance);
                 }
+
                 f.ChainedRules.ForEach(r => AddRule(r.RuleToChain, dict, inheritedChance, condition));
                 break;
             case DropBasedOnExpertMode d:
@@ -120,7 +153,8 @@ public class JsonLoot
                 l.ChainedRules.ForEach(r => AddRule(r.RuleToChain, dict, inheritedChance, newCondition));
                 break;
             case DropOneByOne o:
-                AddItemToDict(dict, condition, o.itemId, o.parameters.ChanceNumerator, o.parameters.ChanceDenominator, o.parameters.MinimumItemDropsCount, o.parameters.MaximumItemDropsCount, inheritedChance);
+                AddItemToDict(dict, condition, o.itemId, o.parameters.ChanceNumerator, o.parameters.ChanceDenominator,
+                    o.parameters.MinimumItemDropsCount, o.parameters.MaximumItemDropsCount, inheritedChance);
                 o.ChainedRules.ForEach(r => AddRule(r.RuleToChain, dict, inheritedChance, condition));
                 break;
             case AlwaysAtleastOneSuccessDropRule a:
@@ -128,6 +162,7 @@ public class JsonLoot
                 {
                     AddRule(r, dict, inheritedChance, condition);
                 }
+
                 a.ChainedRules.ForEach(r => AddRule(r.RuleToChain, dict, inheritedChance, condition));
                 break;
             case HerbBagDropsItemDropRule h:
@@ -135,30 +170,42 @@ public class JsonLoot
                 {
                     AddItemToDict(dict, condition, id, 115, 9, 1, 1, inheritedChance);
                 }
+
                 h.ChainedRules.ForEach(r => AddRule(r.RuleToChain, dict, inheritedChance, condition));
                 break;
             case CoinsRule c:
                 var value = c.value;
                 if (value % 100 != 0L)
                 {
-                    AddItemToDict(dict, condition, ItemID.CopperCoin, inheritedChance, (int)value % 100, (int)value % 100);
+                    AddItemToDict(dict, condition, ItemID.CopperCoin, inheritedChance, (int)value % 100,
+                        (int)value % 100);
                 }
-                if ((value / 100) % 100 != 0L)
+
+                value /= 100;
+                if (value % 100 != 0L)
                 {
-                    AddItemToDict(dict, condition, ItemID.SilverCoin, inheritedChance, (int)(value / 100) % 100, (int)(value / 100) % 100);
+                    AddItemToDict(dict, condition, ItemID.SilverCoin, inheritedChance, (int)value % 100,
+                        (int)value % 100);
                 }
-                if ((value / 10000) % 100 != 0L)
+
+                value /= 100;
+                if (value % 100 != 0L)
                 {
-                    AddItemToDict(dict, condition, ItemID.GoldCoin, inheritedChance, (int)(value / 10000) % 100, (int)(value / 10000) % 100);
+                    AddItemToDict(dict, condition, ItemID.GoldCoin, inheritedChance, (int)value % 100,
+                        (int)value % 100);
                 }
-                if ((value / 1000000) % 100 != 0L)
+
+                value /= 100;
+                if (value > 0L)
                 {
-                    AddItemToDict(dict, condition, ItemID.PlatinumCoin, inheritedChance, (int)(value / 1000000) % 100, (int)(value / 1000000) % 100);
+                    AddItemToDict(dict, condition, ItemID.PlatinumCoin, inheritedChance, (int)value, (int)value);
                 }
+
                 c.ChainedRules.ForEach(r => AddRule(r.RuleToChain, dict, inheritedChance, condition));
                 break;
             case CommonDrop c:
-                AddItemToDict(dict, condition, c.itemId, c.chanceNumerator, c.chanceDenominator, c.amountDroppedMinimum, c.amountDroppedMaximum, inheritedChance);
+                AddItemToDict(dict, condition, c.itemId, c.chanceNumerator, c.chanceDenominator, c.amountDroppedMinimum,
+                    c.amountDroppedMaximum, inheritedChance);
                 c.ChainedRules.ForEach(r => AddRule(r.RuleToChain, dict, inheritedChance, condition));
                 break;
         }
@@ -171,7 +218,8 @@ public class JsonLoot
         public override bool CanRead => false;
         public override bool CanWrite => true;
 
-        public override JsonLoot ReadJson(JsonReader reader, Type objectType, JsonLoot existingValue, bool hasExistingValue,
+        public override JsonLoot ReadJson(JsonReader reader, Type objectType, JsonLoot existingValue,
+            bool hasExistingValue,
             JsonSerializer serializer)
         {
             throw new NotImplementedException();
@@ -185,16 +233,19 @@ public class JsonLoot
                 writer.WritePropertyName("globalLoot");
                 serializer.Serialize(writer, value.globalLoot);
             }
+
             if (!value.NormalEmpty)
             {
                 writer.WritePropertyName("normalLoot");
                 serializer.Serialize(writer, value.normalLoot);
             }
+
             if (!value.ExpertEmpty)
             {
                 writer.WritePropertyName("expertLoot");
                 serializer.Serialize(writer, value.expertLoot);
             }
+
             if (!value.MasterEmpty)
             {
                 writer.WritePropertyName("masterLoot");
